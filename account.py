5695c3b62c17a4e23c6414c3d86697200de4d621
Felipe Romao <feliperomaocad@gmail.com>
2019-11-29 23:55:03 -0300

    Refatorando para usar classe de conta

--- account.py
+++ account.py
@@ -2,16 +2,47 @@ from database import connect
 
 class Account:
 
+    OPERATIONS = [
+        {
+            'option': '1',
+            'name': 'Saldo',
+            'operation': 'show_ballance_operation',
+            'need_is_admin': False
+        },
+        {
+            'option': '2',
+            'name': 'Saque',
+            'operation': 'withdraw_operation',
+            'need_is_admin': False
+        },
+        {
+            'option': '3',
+            'name': 'Deposito',
+            'operation': 'deposit_operation',
+            'need_is_admin': False
+        },
+        {
+            'option': '4',
+            'name': 'Adicionar notas',
+            'operation': 'add_bills',
+            'need_is_admin': True
+        }
+    ]
+
     def __init__(self, account_data):
-        self._id = account_data._id
-        self.number = account_data.number
-        self.balance = account_data.balance
-        self.name = account_data.name
-        self.admin = account_data.admin
+        self._id = account_data['_id']
+        self.number = account_data['number']
+        self.balance = account_data['balance']
+        self.name = account_data['name']
+        self.admin = account_data['admin']
+
+
+    def show_ballance_operation(self):
+        print(f'Saldo em conta é de: {self.balance}')
 
 
     # Executa o saque da conta
-    def withdraw(self, value):
+    def withdraw_operation(self, value):
         if self.balance - value < 0:
             print("Impossivel realizar saque, Saldo insuficiente.")
             return False
@@ -21,7 +52,7 @@ class Account:
 
 
     # Executa o depósito na conta
-    def deposit(self):
+    def deposit_operation(self):
         pass
 
 
@@ -34,5 +65,21 @@ class Account:
         self.balance = value
         #TODO Atualizar documento no banco de dados
         db = connect()
-        db.accounts.update_one({'_id': self._id}, {'balance': self.balance
-        })
+        db.accounts.update_one({'_id': self._id}, {'balance': self.balance})
+
+
+    def show_operations(self):
+        for option in Account.OPERATIONS:
+            operation_string = f'{option["option"]} - {option["name"]}'
+            
+            if option['need_is_admin'] and self.admin or not option['need_is_admin']:
+                print(operation_string)
+
+        return input('Selecione uma das opções acima: ')
+
+    
+    def call_operation(self, selected):
+        for option in Account.OPERATIONS:
+            if option['option'] == selected:
+                operation = getattr(self, option['operation'])
+                operation()
--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from utils import clear, header, invalid_account
 from console import CashMachineConsole
 from auth import Auth
+from account import Account
 
 def main():
     clear()
     header()
 
-    account = False
+    account_auth = False
 
-    while not account:
-        account = Auth.auth_client()
-        if not account:
+    while not account_auth:
+        account_auth = Auth.auth_client()
+        if not account_auth:
             clear()
             header()
             invalid_account()
 
-    CashMachineConsole.call_operation(account)
+    account = Account(account_auth)
 
+    option = account.show_operations()
+    account.call_operation(option)
 
 if __name__ == '__main__':
     while True:
